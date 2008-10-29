eceac1b2efd1fe98433747be406d50f981b2e721
Mikael Hallendal <micke@imendio.com>
2008-10-29 20:30:48 +0100

    Updated Loudmouth requirement to 1.4.0 and added README content.
    Also added the COPYING file containing LGPL.

--- extconf.rb
+++ extconf.rb
@@ -9,7 +9,7 @@ require 'mkmf'
 
 srcdir = File.join(File.expand_path(File.dirname(__FILE__)), 'src')
 
-PKGConfig.have_package("loudmouth-1.0", 1, 3, 4) or exit 1
+PKGConfig.have_package("loudmouth-1.0", 1, 4, 0) or exit 1
 PKGConfig.have_package("glib-2.0", 2, 4, 0) or exit 1
 
 create_makefile("loudmouth", srcdir)
